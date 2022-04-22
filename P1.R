2c691f4d96dea82ee797a878823c1257e5c09c36
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-04-22 21:28:34 +0100

    P1 pivot_longer
    Substitui o segundo loop pela funcao pivot_longer

--- P1.R
+++ P1.R
@@ -5,28 +5,15 @@ residuos <- read_excel('ResiduosPerCapita.xlsx',
                        range = 'A14:C43',
                        col_types = c('text','numeric','numeric'),
                        col_names = c('Grupo','2004','2018'))
-# Organizar os dados
+# Reduzir os nomes dos Grupos às suas iniciais
 for (i in 1:nrow(residuos)) {
   residuos[i,1] <- substr(residuos[i, 1], start = 1 , stop = 2)
 }
 
-dfResiduos <- data.frame(Grupo = rep(0,length.out = 2*nrow(residuos)),
-                         Ano = rep(c('2004','2018'),length.out = 2*nrow(residuos)),
-                         Residuos = rep(0,length.out = 2*nrow(residuos)))
-
-for(i in 1:nrow(residuos)){
-  dfResiduos[2*i-1,1] <- residuos[i, 1]
-  dfResiduos[2*i,1] <- residuos[i, 1]
-
-  dfResiduos[2*i-1,3] <- residuos[i,2]
-  dfResiduos[2*i,3] <- residuos[i,3]
-}
-
-# Fazer o gráfico
-dfResiduos %>%
-  filter(Grupo == 'IT'|
-           Grupo == 'CZ'|
-           Grupo == 'MT') %>%
+tibleResiduos <- as_tibble(residuos)
+tibleResiduos %>%
+  pivot_longer(c(`2004`, `2018`), names_to = "Ano", values_to = "Residuos") %>%
+  filter(Grupo == 'IT'| Grupo == 'CZ'| Grupo == 'MT') %>%
   ggplot(aes(x = Grupo, y = Residuos, fill = Ano)) +
   geom_bar(stat = "identity", position = "dodge")+
   geom_text(aes(label=Residuos), vjust=1.6, color="white",
