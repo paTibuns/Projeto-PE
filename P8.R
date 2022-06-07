0699c7e03eb2cdeb09772e8141616fe76dd7c510
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-06-07 16:20:36 +0100

    final1

--- P8.R
+++ P8.R
@@ -1,8 +1,22 @@
-#Dados iniciai
+#Dados iniciais
 expSeed <- 214
 nAmostras <- 600
 dim <- 1068
 lamb <- 0.3
+lvlConf <- 0.97
 
 set.seed(seed = expSeed)
 amostra <- replicate(nAmostras, rexp(n = dim, rate = lamb))
+media <- apply(amostra,2,mean)
+
+alpha <- 1 - lvlConf
+quantAlpha <- qnorm(1-alpha/2)
+
+limAmp <- array (dim = nAmostras)
+for (i in 1:length(media)){
+  limInf <- ((1-(quantAlpha/sqrt(dim)))/media[i])
+  limSup <- ((1+(quantAlpha/sqrt(dim)))/media[i])
+  limAmp[i] <- limSup - limInf
+}
+res <- mean(limAmp)
+format(round(res, 6), nsmall = 6)
