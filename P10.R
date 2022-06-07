a4be1b242ba81b6b85b3a19f048ee5660d1824f8
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-06-07 20:56:29 +0100

    Tcharaaaam

--- P10.R
+++ P10.R
@@ -3,5 +3,57 @@ library(tidyverse)
 expSeed <- 50
 nAmostras <- 1000
 dimArray <- seq(100, 2500, by = 100)
-lamb <- 4.57
-lvlConf <- 0.97
+lambOrig <- 4.57
+lambCont <- 0.19
+lvlConf <- 0.98
+epsilon <- 0.2
+
+alpha <- 1 - lvlConf
+quantAlpha <- qnorm(1-alpha/2)
+dfGraf <- data.frame(matrix(nrow = 2*length(dimArray),ncol = 3))
+colnames(dfGraf) <- c('dim','type','medAmp')
+
+j <- 0
+for (dim in dimArray){
+  j <- j + 1
+  set.seed(seed = expSeed)
+  amostra <- replicate(nAmostras, rexp(n = dim, rate = lambOrig))
+  media <- apply(amostra,2,mean)
+  limAmp <- array(dim = nAmostras)
+  for (i in 1:nAmostras){
+    limInf <- ((1-(quantAlpha/sqrt(dim)))/media[i])
+    limSup <- ((1+(quantAlpha/sqrt(dim)))/media[i])
+    limAmp[i] <- limSup - limInf
+  }
+  dfGraf[j,1] <- dim
+  dfGraf[j,2] <- 'MAn'
+  dfGraf[j,3] <- mean(limAmp)
+}
+
+for (dim in dimArray){
+  j <- j + 1
+  set.seed(seed = expSeed)
+  amostra <- replicate(nAmostras, rexp(n = dim, rate = lambOrig))
+  outliers <- replicate(nAmostras, rexp(n = dim*epsilon, rate = lambCont))
+  amostra[1:nrow(outliers),1:nAmostras] <- outliers
+  media <- apply(amostra,2,mean)
+  limAmp <- array(dim = nAmostras)
+  for (i in 1:nAmostras){
+    limInf <- ((1-(quantAlpha/sqrt(dim)))/media[i])
+    limSup <- ((1+(quantAlpha/sqrt(dim)))/media[i])
+    limAmp[i] <- limSup - limInf
+  }
+  dfGraf[j,1] <- dim
+  dfGraf[j,2] <- 'MACn'
+  dfGraf[j,3] <- mean(limAmp)
+}
+
+ggplot(dfGraf,aes(x = dim, y = medAmp, color = type))+
+  #geom_line()+
+  geom_point(size = 5, alpha = 0.7)+
+  scale_color_manual(values=c('#ad1519','#008724'))+
+  theme_minimal()+
+  labs(title = 'Media da amplitude do intervalo de confianca vs dimensao da amostra',
+       caption = 'Semente = 50 | m = 1000 | lambda = 4.57 | lambdaC = 0.19 | epsilon = 0.2 | 1 - alfa = 0.98',
+       x = 'Dimensao das m amostras',
+       y = 'Media da amplitude do intervalo de confianca das m amostras')
