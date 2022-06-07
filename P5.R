92b30acf310dd5c1bd51da811ede27a7366deadd
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-06-07 10:17:23 +0100

    Update P5.R

--- P5.R
+++ P5.R
@@ -8,14 +8,11 @@ nDecimais <- 6
 #Gerar Amostra
 set.seed(seed = expSeed)
 amostra <- rexp(n = dim, rate = lamb)
-
 #Probabilidade empirica
 eDist <- ecdf(amostra)
 eProb <- 1 - eDist(tempEsp)   #P(X>4) = 1 - P(X<=4)
-
 #Probabilidade teorica
 tProb <- exp(-(lamb*tempEsp))  #Considerado que P(X>=x) = P(X>x) porque distribuicao é continua
-
 #Diferença entre probabilidades
 res <- abs(tProb-eProb)
 format(round(res, nDecimais), nsmall = nDecimais)
