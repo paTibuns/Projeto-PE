e816aa6e5b9422903649fa539b074ee3579bff83
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-06-10 00:38:06 +0100

    Update P2.R

--- P2.R
+++ P2.R
@@ -2,16 +2,10 @@ library(tidyverse)
 library(readxl)
 library(ggrepel)
 
-#emv -> Esperanca media de vida
 emv <- read_excel('EsperancaVida.xlsx', range = 'A9:CY70', col_names = FALSE)
 tibblEmv <- as_tibble(emv)
 
-#TODO SUBSTITUIR O LOOP POR UMA FORMA MAIS COMPACTA
-#tibblEmv[1,] <-  map_at(tibblEmv,.at = 2:length(tibblEmv[1,]),as.list(sapply(strsplit(as.character(tibblEmv[1,]),split = " "), `[`, 1)))
-#tibblEmv[1,3]
-#map(tibblEmv[1,],str_c(tibblEmv[1,],'T',sep = '_'))
-
-#Reduzir os nomes dos paises às suas iniciais e adicionar uma coluna com o sexo e pais correspondente ao valor da emv
+#Reduzir os nomes dos paises as suas iniciais e adicionar uma coluna com o sexo e pais correspondente ao valor da emv
 for (i in 2:ncol(tibblEmv)) {
   tibblEmv[1,i] <- sapply(strsplit(as.character(tibblEmv[1,i]),split = " "), `[`, 1)
   if(i<36) {
@@ -25,30 +19,25 @@ for (i in 2:ncol(tibblEmv)) {
   }
 }
 
-#Adicionar os nomes das colunas aos dados
 colnames(tibblEmv) <- tibblEmv[1,]
 colnames(tibblEmv)[1] <- 'Ano'
 tibblEmv <- slice(tibblEmv,-1)
 
-#Separar a coluna dos sexos e paises em 2 (uma coluna para cada)
 tibblEmv %>%
-  pivot_longer(UE27_Total:CH_Mulheres,names_to = 'GS',values_to = 'EMV',values_transform = list(EMV = as.numeric)) %>%
-    separate(GS,c('Grupo','Sexo'),sep = '_') %>%
-
-#Fazer o gráfico
+  #Separar a coluna dos sexos e paises em 2 (uma coluna para cada)
+  pivot_longer(UE27_Total:CH_Mulheres,names_to = 'GS',values_to = 'EMV',
+               values_transform = list(EMV = as.numeric)) %>%
+  separate(GS,c('Grupo','Sexo'),sep = '_') %>%
+  
   filter((Grupo == 'ES'| Grupo == 'GR'|Grupo == 'HU') &
            (between(Ano,2002,2019)) &
            (Sexo == 'Homens'|Sexo == 'Mulheres')) %>%
   ggplot(aes(x = Ano, y = EMV,colour = Grupo,shape = Sexo))+
-  #ggrepel::geom_label_repel(aes(label =  max(tibblEmv$EMV)),  #---> tentativa de criar labels com os
-  #                          size = 6,                          #    valores dos pontos
-  #                          label.size = 0,
-  #                          segment.color = NA)+
   geom_point(size = 5, alpha = 0.7)+
   geom_smooth(se = F)+
   theme_minimal()+
   scale_color_manual(values=c("#ad1519", "#001489",'#008724'))+
-  labs(title = 'Esperanca media de vida por grupo e sexo aumentou ao longo dos anos',
+  labs(title = 'Esperanca media de vida por grupo e sexo entre 2002 e 2019',
        caption = 'Dados obtidos de pordata.pt',
-       x = 'Ano da estimativa', #Ou 'Momento de referencia'?
+       x = 'Ano da estimativa',
        y = 'Esperanca Media de Vida [Anos]')
