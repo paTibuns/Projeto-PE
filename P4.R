152b660d8310e7e29fe9b537a324d404bbf49db8
paTibuns <pedro.r.pereira@tecnico.ulisboa.pt>
2022-06-09 20:33:09 +0100

    Update P4.R

--- P4.R
+++ P4.R
@@ -7,8 +7,7 @@ tibblUten <- as_tibble(dfUten)
 tibblUten %>%
   ggplot(aes(x = IMC,y = TAD))+
   geom_point(size = 3)+
-  geom_density_2d()+ #---> Desenho de isolinhas
-  #geom_smooth(method = "auto", se = TRUE, fullrange = FALSE, level = 0.95)+ # ---> Regressao linear
+  geom_density_2d()+
   labs(title = 'Grafico de dispersao entre as variaveis IMC e TAD',
        x = 'IMC - Indice de Massa Corporal',
        y = 'TAD - Tensao Arterial Diastolica')
